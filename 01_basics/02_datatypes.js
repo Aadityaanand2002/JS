18fe0e841ceadef82d2e910eb8882c012169bf2f
Aadityaanand2002 <adityaanand15902@gmail.com>
2025-05-30 13:59:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs not browser
+
+console.log(3 + 3);
+
+let isLoggedIn = false; //bool
+
+// null -> standalone value
+
+let age = 2
+
+console.log(typeof age)
